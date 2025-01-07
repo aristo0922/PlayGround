ff2fdcc12acdc1143178e53403a4b7de3f631f71
aristo <dkfud2121@naver.com>
2025-01-07 23:25:33 +0900

    feat: add reservation lists in party class

--- PlayGround/src/main/java/com/villain/play/ground/PlayGround/party/Party.java
+++ PlayGround/src/main/java/com/villain/play/ground/PlayGround/party/Party.java
@@ -1,6 +1,8 @@
 package com.villain.play.ground.PlayGround.party;
 
+import com.villain.play.ground.PlayGround.reservation.Reservation;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -14,8 +16,23 @@ public class Party {
   private int limit;
   private int now;
 
+  private List<Reservation> reservationList;
 //  private LocalDateTime start;
 //  private LocalDateTime end;
+
+  public Party(int id, String platform, String album, String leader, int recruit, int limit, int now){
+    this.id = id;
+    this.platform=platform;
+    this.album = album;
+    this.leader = leader;
+    this.recruit = recruit;
+    this.limit = limit;
+    this.now = now;
+  }
+
+  public void addReservation(Reservation reservation){
+    reservationList.add(reservation);
+  }
 
   public boolean isFull() {
     return limit <= now ? true : false;
@@ -23,4 +40,6 @@ public class Party {
   public int getId(){
     return this.id;
   }
+
+
 }
