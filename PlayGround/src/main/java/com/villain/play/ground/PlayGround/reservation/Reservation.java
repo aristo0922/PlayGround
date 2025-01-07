2262ef01d2cc69fda50d882a40f9a33ee1b657f6
aristo <dkfud2121@naver.com>
2025-01-07 23:31:15 +0900

    feat: reservation 객체 선입금 여부 필드 분리

--- PlayGround/src/main/java/com/villain/play/ground/PlayGround/reservation/Reservation.java
+++ PlayGround/src/main/java/com/villain/play/ground/PlayGround/reservation/Reservation.java
@@ -4,27 +4,18 @@ public class Reservation {
 
   private String member;
   private String user;
-  private boolean isDeposit; // 객체에서 분리시키는게 좋을 것 같은데
-
 
   public static class Builder{
     String member;
     String user;
-    int party;
-    boolean isDeposit;
 
     public Builder member(String member){
       this.member = member;
       return this;
     }
 
-    public Builder user(String user){
+    public Builder user(String user) {
       this.user = user;
-      return this;
-    }
-
-    public Builder isDeposit(boolean isDeposit){
-      this.isDeposit = isDeposit;
       return this;
     }
 
@@ -36,6 +27,5 @@ public class Reservation {
   private Reservation(Builder builder){
     this.member = builder.member;
     this.user = builder.user;
-    this.isDeposit = builder.isDeposit;
   }
 }
