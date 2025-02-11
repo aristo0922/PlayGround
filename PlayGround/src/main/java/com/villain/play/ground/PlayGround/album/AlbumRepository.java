8c642a0a9af62df3590183123e031f391b8afdf2
aristo <dkfud2121@naver.com>
2025-02-11 12:25:16 +0900

    feat: create AlbumRepository extends JpaRepository

---
+++ PlayGround/src/main/java/com/villain/play/ground/PlayGround/album/AlbumRepository.java
@@ -0,0 +1,9 @@
+package com.villain.play.ground.PlayGround.album;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+}
